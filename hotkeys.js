d0920bea6f38888f14cc7779bbb8ba900c889744
Chebotarev <nosilence@list.ru>
2021-06-15 17:17:09 +0300

    Greasemonkey header added

--- hotkeys.js
+++ hotkeys.js
@@ -1,8 +1,15 @@
+// ==UserScript==
+// @name     Simple Form Save HotKey
+// @version  1
+// @include *://*.simpleone.ru/record/*
+// @include *://localhost:3000/record/*
+// ==/UserScript==
+
 window.addEventListener('keyup', (e) => {
-	if (e.altKey && e.shiftKey && (e.keyCode == 83)){
-		const saveButton = document.querySelector('[data-test="form_button__save-ui-button"]');
-		if (saveButton) {
-			saveButton.click();
-		}
-	}
+    if (e.altKey && e.shiftKey && (e.keyCode == 83)) {
+        const saveButton = document.querySelector('[data-test="form_button__save-ui-button"]');
+        if (saveButton) {
+            saveButton.click();
+        }
+    }
 }, false);
